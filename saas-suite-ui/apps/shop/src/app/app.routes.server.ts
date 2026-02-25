0c6903b6a96b0857ad9f2f0d49df3ab92495e426
felipericarte <felipericartem@gmail.com>
2026-02-24 21:06:16 -0300

    fix: estrutura de arquivos e readme

--- saas-suite-ui/apps/shop/src/app/app.routes.server.ts
+++ saas-suite-ui/apps/shop/src/app/app.routes.server.ts
@@ -1,12 +1,11 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: 'products/:id',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender,
-  },
+  { path: 'product/:id', renderMode: RenderMode.Server },
+  { path: 'product', renderMode: RenderMode.Server },
+  { path: 'products', renderMode: RenderMode.Server },
+  { path: 'products/**', renderMode: RenderMode.Server },
+  { path: 'checkout', renderMode: RenderMode.Server },
+  { path: 'orders', renderMode: RenderMode.Server },
+  { path: '**', renderMode: RenderMode.Prerender },
 ];
